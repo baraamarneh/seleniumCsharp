5fef69ace55cbcb9650b37dbf2c5ca8d883b5971
Bara.Amarneh <bara.amarneh@catonetworks.com>
2025-01-22 20:13:38 +0200

    clean up code

--- tests/LoginTests.cs
+++ tests/LoginTests.cs
@@ -1,9 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
 using SeleniumCsharp.pages;
-using System;
 namespace SeleniumCsharp.tests
 {
     public class Tests
